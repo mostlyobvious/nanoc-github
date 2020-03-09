8808bbc0fb89d5aeae6461bbf75d78dc36eddfa2
Paweł Pacana <pawel.pacana@gmail.com>
2020-03-10 00:19:58 +0100

    Parse post metadata from frontmatter.

--- lib/nanoc/git.rb
+++ lib/nanoc/git.rb
@@ -1,6 +1,7 @@
 require "nanoc/git/version"
 require "nanoc"
 require "octokit"
+require "front_matter_parser"
 
 # GITHUB_TOKEN i.e. 40-char string from https://github.com/settings/tokens
 # GITHUB_REPO  i.e. arkency/blog.arkency.com
@@ -18,7 +19,11 @@ module Nanoc
           .contents(ENV['GITHUB_REPO'])
           .select { |item| item.end_with?(".md") }
           .map    { |item| client.contents(ENV['GITHUB_REPO'], path: item[:path]) }
-          .map    { |item| new_item(item[:content], item, Nanoc::Identifier.new(item[:path])) }
+          .map    { |item| new_item(content = item[:content], parse_frontmatter(content), Nanoc::Identifier.new(item[:path])) }
+      end
+
+      def parse_frontmatter(content)
+        FrontMatterParser::Parser.new(:md).call(content).front_matter
       end
     end
   end
--- nanoc-git.gemspec
+++ nanoc-git.gemspec
@@ -37,6 +37,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "nanoc", "~> 4.0"
   spec.add_dependency "octokit", "~> 4.0"
+  spec.add_dependency "front_matter_parser", "= 0.2.1"
 
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
