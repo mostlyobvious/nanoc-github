108d955a56fc0d1102ea39d9305c86a2a2897214
Paweł Pacana <pawel.pacana@gmail.com>
2020-03-09 23:35:56 +0100

    Simplest thing that could work.

--- lib/nanoc/git.rb
+++ lib/nanoc/git.rb
@@ -1,6 +1,9 @@
 require "nanoc/git/version"
 require "nanoc"
 
+# GITHUB_TOKEN i.e. 40-char string from https://github.com/settings/tokens
+# GITHUB_REPO  i.e. arkency/blog.arkency.com
+
 module Nanoc
   module Git
     class Error < StandardError; end
@@ -9,7 +12,12 @@ module Nanoc
       identifier :git
 
       def items
-        []
+        client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+        client
+          .contents(ENV['GITHUB_REPO'])
+          .select { |item| item.end_with?(".md") }
+          .map    { |item| client.contents(ENV['GITHUB_REPO'], path: item[:path]) }
+          .map    { |item| new_item(item[:content], item, Nanoc::Identifier.new(item[:path])) }
       end
     end
   end
